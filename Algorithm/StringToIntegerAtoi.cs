cf251995ab1ed7b3ce0aa5d51fafd165aabf6cc4
Ifeoluwa Gade <ifeoluwaosungade@gmail.com>
2020-07-01 11:11:30 +0100

    Implement string to inter Atoi

--- Algorithm/StringToIntegerAtoi.cs
+++ Algorithm/StringToIntegerAtoi.cs
@@ -7,7 +7,7 @@ namespace Algorithm
     {
         public StringToIntegerAtoi()
         {
-            // Console.WriteLine(ATOI("    -42"));
+             Console.WriteLine(sATOI("-91283472332"));
 
             Console.WriteLine(sATOI("   42"));
 
@@ -24,55 +24,86 @@ namespace Algorithm
             for (int i = 0; i < str.Length; i++)
             {
                 if (!char.IsWhiteSpace(str[i]))
-                    return convertToInteger(str.Substring(i));
+                    return GetInt(str.Substring(i));
 
             }
 
             return 0;
         }
 
-        private int convertToInteger(string v)
+        private int GetInt(string value)
         {
-            return 0;
-        }
+            double output = 0;
+            int indicator = 1;
+            int i = 0;
 
-        int ATOI(string str)
-        {
-
-            str = str.Trim();
-            if (string.IsNullOrEmpty(str) || (str[0] == '+' || str[0] == ' '))
-                return 0;
-            string output = string.Empty;
-            for (int i = 0; i < str.Length; i++)
+            if(value[0] == '-' || value[0] == '+')
             {
-
-                if (i == 0 && !char.IsDigit(str[i]) &&
-                    !char.IsWhiteSpace(str[i]) && str[i] != '-'
-                    || (str.Length == 1 && str[i] == '+') || (str.Length == 1 && str[i] == '-'))
-                {
-                    output = "0";
-                    break;
-                }
-
-                if (output.Length == 0 && (str[i] == '+' || str[i] == '-'))
-                    output = output + str[i];
-
-                if (char.IsDigit(str[i]))
-                    output = output + str[i];
-
-                if (i < str.Length - 1 && char.IsDigit(str[i]) && !char.IsDigit(str[i + 1]))
-                    break;
+                if (value[0] == '-')
+                    indicator = -1;
+                else
+                    indicator = 1;
+                i++;
             }
 
-            int num = 0;
+            while(i < value.Length)
+            {            
+                if (value[i] >= '0' && value[i] <= '9')
+                {
+                    output = output * 10 + (value[i] - '0');
+                }
+                else
+                    break;
+                i++;
+            }
+            if (indicator == -1)
+                output = -output;
 
-            if (int.TryParse(output, out num))
-                return num;
-
-            if (num > int.MaxValue)
+            if (output > int.MaxValue)
                 return int.MaxValue;
+            if (output < int.MinValue)
+                return int.MinValue;
 
-            return int.MinValue;
+            return (int) output;
         }
+
+        //int ATOI(string str)
+        //{
+
+        //    str = str.Trim();
+        //    if (string.IsNullOrEmpty(str) || (str[0] == '+' || str[0] == ' '))
+        //        return 0;
+        //    string output = string.Empty;
+        //    for (int i = 0; i < str.Length; i++)
+        //    {
+
+        //        if (i == 0 && !char.IsDigit(str[i]) &&
+        //            !char.IsWhiteSpace(str[i]) && str[i] != '-'
+        //            || (str.Length == 1 && str[i] == '+') || (str.Length == 1 && str[i] == '-'))
+        //        {
+        //            output = "0";
+        //            break;
+        //        }
+
+        //        if (output.Length == 0 && (str[i] == '+' || str[i] == '-'))
+        //            output = output + str[i];
+
+        //        if (char.IsDigit(str[i]))
+        //            output = output + str[i];
+
+        //        if (i < str.Length - 1 && char.IsDigit(str[i]) && !char.IsDigit(str[i + 1]))
+        //            break;
+        //    }
+
+        //    int num = 0;
+
+        //    if (int.TryParse(output, out num))
+        //        return num;
+
+        //    if (num > int.MaxValue)
+        //        return int.MaxValue;
+
+        //    return int.MinValue;
+        //}
     }
 }
