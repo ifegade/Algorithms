40f782a65c6193ebaefcc92afaebf5fceee4166a
ifeoluwa <ifeoluwaosungade@gmail.com>
2020-07-01 20:19:31 +0100

    Solved Arrange Binary Search

--- Algorithm/ArrangeCoin.cs
+++ Algorithm/ArrangeCoin.cs
@@ -8,30 +8,72 @@ namespace Algorithm
     {
         public ArrangeCoin()
         {
-            Console.WriteLine( completeStep(5));
+            Console.WriteLine(UsingBinanrySearch(5));
         }
 
-        private int completeStep(int num)
+
+        private int UsingBinanrySearch(int n)
         {
-            Stack<int> rows = new Stack<int>();
-            int count = 0;
-            int numberOfStaris = 0;
-            for(int i = 1; i <= num; i++)
+            long left = 0;
+            long right = n;
+            long k = 0;
+            long current = 0;
+            while (left <= right)
             {
-                for(int j = 0; j < i; j++)
-                {
-                    if(count == num)
-                    {
-                            return numberOfStaris;
-                    }
-                    Console.Write("*");                   
-                    count++;
-                }
-                numberOfStaris++;
-                Console.WriteLine();
+                k = left + (right - left) / 2;
+
+                current = k * (k + 1) / 2;
+
+                if (current == n) return (int)k;
+
+                if (n < current)
+                    right = k - 1;
+                else
+                    left = k + 1;
             }
 
-            return numberOfStaris;
+            return (int)right;
+        }
+
+        private int completeStep(int n)
+        {
+            int levelCounter = 0;
+            int row = 1;
+
+            if (n == 0)
+                return 0;
+
+            while (n >= row)
+            {
+                n = n - row;
+                row += 1;
+                levelCounter += 1;
+            }
+
+            return levelCounter;
+
+            //will fail for large numbers
+            // return (int)(Math.sqrt(2 * (long)n + 0.25) - 0.5);
+
+
+            //not efficient enough
+            //int count = 0;
+            //int numberOfStaris = 0;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        if (count == n)
+            //        {
+            //            return numberOfStaris;
+            //        }
+
+            //        count++;
+            //    }
+            //    numberOfStaris++;
+            //}
+
+            //return numberOfStaris;
         }
     }
 }
